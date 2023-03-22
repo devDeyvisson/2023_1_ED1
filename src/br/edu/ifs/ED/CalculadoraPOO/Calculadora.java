78b3cfce6e154fa22001a7603c0da6533dda3bb8
Qen <118078373+WezleyRenan@users.noreply.github.com>
2023-03-21 21:04:46 -0300

    resumo de POO 2 questao

--- src/br/edu/ifs/ED/CalculadoraPOO/Calculadora.java
+++ src/br/edu/ifs/ED/CalculadoraPOO/Calculadora.java
@@ -1,45 +1,40 @@
-package RevisaoPOO;
+package Controle;
 
 import java.util.Scanner;
 
-public class Calculadora {
+public class Calculadora extends Resultado {
 
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
 		
-		double resultado;
-			
+		Resultado resultado = new Resultado();
+		System.out.println(Menu.getMenu());
+		int opcao=input.nextInt();
+		
 		System.out.println("Informe dois números: ");
 		double numero1 = input.nextDouble(); 
 		double numero2 = input.nextDouble();
 		
-		System.out.println("Escolha o tipo de operação:");
-		int escolha = input.nextInt();
-		
-		switch(escolha) {
-			case 1:
-				resultado = numero1 + numero2;
-				System.out.println("O resultado foi : " + resultado);
-			break;
-				
-			case 2:
-				resultado = numero1 - numero2;
-				System.out.println("O resultado foi : " + resultado);
+		switch (opcao) {
+			case 1: 
+				System.out.println (resultado.soma(numero1, numero2));
 			break;
 			
-			case 3:
-				resultado = numero1 * numero2;
-				System.out.println("O resultado foi : " + resultado);
+			case 2: 
+				System.out.println (resultado.subtracao(numero1, numero2));
 			break;
 			
-			case 4:
-				resultado = numero1 / numero2;
-				System.out.println("O resultado foi : " + resultado);
+			case 3: 
+				System.out.println (resultado.multiplicacao(numero1, numero2));
 			break;
 			
+			case 4: 
+				System.out.println (resultado.divisao(numero1, numero2));
+			break;
 		}
 		
 	}
 
 }
+
