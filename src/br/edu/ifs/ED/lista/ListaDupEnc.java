4c78e316c71bd0986136cb1eeb339a981c446e80
Deyvisson Menezes <113800984+devDeyvisson@users.noreply.github.com>
2023-05-25 16:56:39 -0300

    presepada

--- src/br/edu/ifs/ED/lista/ListaDupEnc.java
+++ src/br/edu/ifs/ED/lista/ListaDupEnc.java
@@ -15,7 +15,37 @@ public class ListaDupEnc<T extends Comparable<T>> extends Lista<T>{
 
     @Override
     public void incluir(T elemento, int posicao) throws Exception {
-        throw new Exception("Não implementado");
+        if (posicao < 0 || posicao > tamanho) {
+            throw new Exception("Posição inválida");
+        }
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        if (posicao == 0) {
+            if (inicio != null) {
+                novoNo.prox = Inicio;
+                inicio.ant = novoNo;
+            }
+            Inicio = novoNo;
+        } else {
+            NoDuplo<T> atual = Inicio;
+            int contador = 0;
+        }
+        while (contador < posicao - 1 && atual != null){
+            atual = atual.prox;
+            contador++;
+         }
+         if (atual != null) {
+            novoNo.prox = atual.prox;
+            if (atual.prox != null) {
+                atual.prox.ant = novoNo;
+            }
+        novoNo.ant = atual;
+        atual.prox = novoNo;
+        } else {
+            throw new Exception("Posição inválida");
+        }
+        }
+        tamanho++;
+         }
 
     }
 
@@ -52,4 +82,24 @@ public class ListaDupEnc<T extends Comparable<T>> extends Lista<T>{
         throw new Exception("Não implementado");
 
     }
+    public abstract Lista<T> subLista(int posInicial, int posFinal)
+			throws Exception {
+			 if (posInicial < 0 || posInicial >= tamanho || posFinal < 0 ||
+			posFinal >= tamanho || posInicial > posFinal) {
+				 throw new Exception("Posições inválidas.");
+			 }
+			 Lista<T> subLista = new Lista<>();
+			 NoDuplo<T> atual = inicio;
+			 int cont = 0;
+			 while (cont < posInicial && atual != null) {
+				 atual = atual.prox;
+				 cont++;
+			 }
+			 while (cont <= posFinal && atual != null) {
+				 subLista.incluirFim(atual.dado);
+				 atual = atual.prox;
+				 cont++;
+			 }
+			 return subLista;
+			}
 }
